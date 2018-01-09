shivatejapeddi/packages_apps_Settings-1

Settings: Add back Citrus-CAF Device Maintainers
* Cardinal edits:
  Adapt to new Oreo settings

Change-Id: Id752ba90c7f6509e28591575ca850bbb6c75e86d

src/com/android/settings/DeviceInfoSettings.java
@@ -31,6 +31,7 @@ import com.android.settings.deviceinfo.AdditionalSystemUpdatePreferenceControlle
 import com.android.settings.deviceinfo.BasebandVersionPreferenceController;
 import com.android.settings.deviceinfo.BuildNumberPreferenceController;
 import com.android.settings.deviceinfo.CitrusVersionPreference;
+import com.android.settings.deviceinfo.DeviceMaintainerPreference;
 import com.android.settings.deviceinfo.DeviceModelPreferenceController;
 import com.android.settings.deviceinfo.FccEquipmentIdPreferenceController;
 import com.android.settings.deviceinfo.FirmwareVersionPreferenceController;
@@ -131,6 +132,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         controllers.add(new SELinuxStatusPreferenceController(context));
         controllers.add(new SafetyInfoPreferenceController(context));
         controllers.add(new CitrusVersionPreference(context));
+        controllers.add(new DeviceMaintainerPreference(context));
         return controllers;
     }
 

Kajak2137/packages_apps_Settings

Change-Id: I122f29ad9d24eea6e69e0cc228639f0b5256f1b2
Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

src/com/android/settings/DeviceInfoSettings.java
@@ -30,6 +30,7 @@ import com.android.settings.deviceinfo.BasebandVersionPreferenceController;
 import com.android.settings.deviceinfo.BuildNumberPreferenceController;
 import com.android.settings.deviceinfo.CAFRevisionPreferenceController;
 import com.android.settings.deviceinfo.CitrusVersionPreferenceController;
+import com.android.settings.deviceinfo.DeviceMaintainerPreferenceController;
 import com.android.settings.deviceinfo.DeviceModelPreferenceController;
 import com.android.settings.deviceinfo.FccEquipmentIdPreferenceController;
 import com.android.settings.deviceinfo.FirmwareVersionPreferenceController;
@@ -133,6 +134,7 @@ public class DeviceInfoSettings extends DashboardFragment implements Indexable {
         controllers.add(new SafetyInfoPreferenceController(context));
         controllers.add(new CAFRevisionPreferenceController(context));
         controllers.add(new CitrusVersionPreferenceController(context));
+        controllers.add(new DeviceMaintainerPreferenceController(context));
         return controllers;
     }
 